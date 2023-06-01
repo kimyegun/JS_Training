0f6f522e8a653ed2eb6fab28749eddae95662286
kimyegun123 <kimyegun123@gmail.com>
2023-06-01 17:52:09 +0900

    Java_Script(20/200)

---
+++ Day2/JS01_11.js
@@ -0,0 +1,10 @@
+var number = 2;
+if (number == 1) {
+    console.log('number 1 입니다.')
+} else if (number == 2) {
+    console.log('number 2 입니다.')
+} else if (number == 3) {
+    console.log('number 3 입니다.')
+} else {
+    console.log('number 1,2,3 중 해당되는 것이 없습니다.')
+}
---
+++ Day2/JS02_19.js
@@ -0,0 +1,6 @@
+var value1 = null;
+console.log(value1);
+console.log(typeof value1);
+var value2;
+console.log(value2);
+console.log(typeof value2);
